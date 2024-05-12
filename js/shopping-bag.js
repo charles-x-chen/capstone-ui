6fdf4b6d9e25f8e7cc69a31176921719d9978e86
xiaoyu-chen <xiaoyu.chen@vodafone.com>
2024-05-13 00:19:54 +0100

    working on shopping cart page

--- js/shopping-bag.js
+++ js/shopping-bag.js
@@ -36,17 +36,6 @@ function signInClicked() {
 
 }
 
-function setCategoryList(categories) {
-
-    var innerHtml = "<ul class='category-list'>"
-    for (let i = 0; i < categories.length; i++) {
-        innerHtml = innerHtml + '<li><input type="checkbox" id="cat-' + i + '" name="cat-' + i + '" value="cat-"' + i + '>' + categories[i] + "</input></li>";
-    }
-    innerHtml = innerHtml + "</ul>";
-
-    var x = document.getElementById("categoriesList");
-    x.innerHTML = innerHtml;
-}
 
 function genHtmlForProduct(pJson) {
     var gridDiv = document.createElement('div');
@@ -86,100 +75,147 @@ function genHtmlForProduct(pJson) {
     return gridDiv;
 }
 
-function showProductInfo(productJson) {
-    var x = document.getElementById("infoProductTitle");
-    x.innerText = productJson.title;
+function showProductPricingSummary(productJson) {
 
-    x = document.getElementById("infoProductDescription");
-    x.innerText = productJson.description;
 
 }
 
-function showProductDetails(productJson) {
-    var x = document.getElementById("productTitle");
-    x.innerText = productJson.title;
+// each productJson has a 'productId' and a 'quantity'
+function genProductDetailsDiv(parentDiv, cartProductJson) {
+    // first, we get the product details by its ID
+    fetch('https://fakestoreapi.com/products/' + cartProductJson.productId)
+        .then(res => res.json())
+        .then(json => loadProductDetails(parentDiv, json, cartProductJson.quantity));
+}
 
-    x = document.getElementById("productPrice");
-    x.innerText = "$" + productJson.price;
+function loadProductDetails(parentDiv, productJson, quantity) {
+    // now use the details to build the div structure
+    var prodDiv = document.createElement("div");
+    prodDiv.setAttribute("id", "inBagProductId-" + productJson.id);
+    prodDiv.classList.add("aem-Grid");
+    prodDiv.classList.add("aem-Grid--12");
+    prodDiv.classList.add("in-bag-product-details");
 
-    x = document.getElementById("productDescription");
-    x.innerText = productJson.description;
+    // image div
+    var imgDiv = document.createElement("div");
+    imgDiv.setAttribute("id", "inBagProductImage-" + productJson.id);
+    imgDiv.classList.add("aem-GridColumn");
+    imgDiv.classList.add("aem-GridColumn--default--3");
+    imgDiv.classList.add("in-bag-product-image");
 
-    var ratingContainer = document.getElementById("productRating");
-    const rating = Math.round(productJson.rating.rate);
-    var spanElem;
-    for (let i=0; i<rating; i++) {
-        spanElem = document.createElement("span");
-        spanElem.classList.add("fa");
-        spanElem.classList.add("fa-star");
-        ratingContainer.append(spanElem);
+    var img = document.createElement("img");
+    img.setAttribute("src", productJson.image);
+    img.setAttribute("alt", "");
+    img.setAttribute("width", "100%");
+    img.setAttribute("height", "100%");
+    imgDiv.append(img);
+    prodDiv.append(imgDiv);
+
+    // info
+    var infoDiv = document.createElement("div");
+    infoDiv.setAttribute("id", "inBagProductInfo-" + productJson.id);
+    infoDiv.classList.add("aem-GridColumn");
+    infoDiv.classList.add("aem-GridColumn--default--3");
+    infoDiv.classList.add("in-bag-product-info");
+
+    var heading = document.createElement("h3")
+    heading.setAttribute("id", "productTitle-" + productJson.id);
+    heading.innerText = productJson.title;
+    infoDiv.append(heading);
+
+    heading = document.createElement("h4")
+    heading.setAttribute("id", "productSize-" + productJson.id);
+    heading.innerText = "Size: medium";
+    infoDiv.append(heading);
+
+    heading = document.createElement("h4")
+    heading.setAttribute("id", "productColor-" + productJson.id);
+    heading.innerText = "Color: Storm";
+    infoDiv.append(heading);
+
+    heading = document.createElement("h4")
+    heading.setAttribute("id", "productPrice-" + productJson.id);
+    heading.innerText = "$" + productJson.price;
+    infoDiv.append(heading);
+    prodDiv.append(infoDiv);
+
+
+    // quantity selector
+    var quantityDiv = document.createElement("div");
+    quantityDiv.setAttribute("id", "inBagSelector");
+    quantityDiv.classList.add("aem-GridColumn");
+    quantityDiv.classList.add("aem-GridColumn--default--3");
+    quantityDiv.classList.add("in-bag-selector");
+    quantityDiv.innerHTML = " <div id='productSelect' class='product-select'>\n" +
+        "                            <i class='fa fa-minus-circle remove-bg' aria-hidden='true'></i>\n" +
+        "                            <input id='numProducts' class='num-products' type='number' name='numOfProducts' value='" + quantity + "'/>\n" +
+        "                            <i class='fa fa-plus-circle add-bg' aria-hidden='true'></i>\n" +
+        "                        </div>";
+
+    prodDiv.append(quantityDiv);
+
+    // actions
+    var actionDiv = document.createElement("div");
+    actionDiv.setAttribute("id", "inBagAction");
+    actionDiv.classList.add("aem-GridColumn");
+    actionDiv.classList.add("aem-GridColumn--default--3");
+    actionDiv.classList.add("in-bag-action");
+    actionDiv.innerHTML = " <div class='action-container'>\n" +
+        "                            <div>\n" +
+        "                                <i class='fa fa-pencil remove-bg' aria-hidden='true'></i>\n" +
+        "                                <span>Edit item</span>\n" +
+        "                            </div>\n" +
+        "                            <div>\n" +
+        "                                <i class='fa fa-trash remove-bg' aria-hidden='true'></i>\n" +
+        "                                <span>Remove</span>\n" +
+        "\n" +
+        "                            </div>\n" +
+        "                            <div>\n" +
+        "                                <i class='fa fa-heart-o remove-bg' aria-hidden='true'></i>\n" +
+        "                                <span>Save for later</span>\n" +
+        "                            </div>";
+
+
+    prodDiv.append(actionDiv);
+
+    // add this product div to parent div.
+    parentDiv.append(prodDiv);
+
+}
+
+function genShoppingBagPage(cartJson) {
+    // cartJson is a list of JSON objects for products in the Cart.
+    var listContainer = document.getElementById("inBagProductList");
+    const productList = cartJson[0].products;
+
+    for (let i = 0; i < productList.length; i++) {
+        const prodJson = productList[i];
+        genProductDetailsDiv(listContainer, prodJson);
     }
-    for (let i=rating; i<5; i++) {
-        spanElem = document.createElement("span");
-        spanElem.classList.add("fa");
-        spanElem.classList.add("fa-star-o");
-        ratingContainer.append(spanElem);
-    }
-    spanElem = document.createElement("span");
-    spanElem.innerText = '('+productJson.rating.count+')';
-    ratingContainer.append(spanElem);
-}
 
-function showProductImageList(productJson) {
-    var listContainer = document.getElementById("productImageList");
-    for (let i=0;i<5;++i)  {
-        var a = document.createElement("a");
-        a.setAttribute("href", "#");
-        var img = document.createElement("img");
-        img.setAttribute("src", productJson.image);
-        img.setAttribute("width", "100px");
-        img.setAttribute("height", "120px");
-        img.setAttribute("alt", "");
-        img.classList.add("list-image-bg");
-        a.append(img)
-        listContainer.prepend(a);
-    }
-}
-
-function showProductGrid(category) {
-    console.log(category);
-    fetch('https://fakestoreapi.com/products/category/' + category)
-        .then(res=>res.json())
-        .then(json=>genProductGrid(json));
-}
-
-function loadProductDetails(productJson) {
-
-
-    var x = document.getElementById("productImage");
-    x.setAttribute("src", productJson.image);
-
-    showProductImageList(productJson);
-
-    showProductDetails(productJson);
-
-    showProductInfo(productJson);
+    showProductPricingSummary(cartJson);
 
 
 }
 
 
-window.onload = function productDetailsPageLoad() {
+window.onload = function shoppingBagPageLoad() {
     const currentUrl = window.location.href;
     console.log(currentUrl);
 
     const params = new URLSearchParams(currentUrl);
     console.log(params.keys());
 
-    var productId = 1;
-    if (params.has('productId')) {
-        productId = params.get('productId');
+    // getting cart details for user (userID=1) -->
+    let userId = 1;
+    if (params.has('userId')) {
+        userId = params.get("userId");
     }
-    console.log(productId);
+    console.log(userId);
 
-    fetch('https://fakestoreapi.com/products/'+productId)
+    fetch('https://fakestoreapi.com/carts/user/' + userId)
         .then(res=>res.json())
-        .then(json=>loadProductDetails(json));
+        .then(json=>genShoppingBagPage(json));
 }
 
 
